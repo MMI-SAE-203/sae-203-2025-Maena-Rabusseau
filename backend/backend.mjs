0bbd7d27d5bfe892e173b06fe6c2cb4eb45d5a43
Maena-Rabusseau <maena.rabusseau@edu.univ-fcomte.fr>
2025-03-10 19:01:27 +0100

    Ajout des fonctions de pocketbase

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -12,13 +12,133 @@ const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function Allevents() {
     const record = await pb.collection('Films').getFullList();
-    return record;}
+    return record;
+}
 
 
 
 // une fonction qui retourne la liste de tous les films triés par date de projection
 
-export async function sortedfilms () {
-    const records =  await pb.collection('Films').getFullList({ sort: 'projection', });
+export async function sortedfilms() {
+    const records = await pb.collection('Films').getFullList({ sort: 'projection', });
     return records;
 }
+
+
+// une fonction qui retourne la liste des activités (Ateliers, expositions, animations…) triés par date de projection    
+
+export async function AllActivitessorted() {
+    const records = await pb.collection('Activites').getFullList({ sort: 'Date_activite', });
+    return records;
+}
+
+
+//une fonction qui retourne la liste de tous les acteurs / réalisateurs participant au festival triés par ordre alphabétique
+
+export async function AllInvitessorted() {
+    const records = await pb.collection('Invites').getFullList({ sort: 'Prenom', });
+    return records;
+}
+
+//une fonction qui retourne les infos d'un film en donnant son id en paramètre
+
+export async function oneIDFilm(id) {
+    const Onerecords = await pb.collection('Films').getOne(id);
+    return Onerecords;
+}
+
+//une fonction qui retourne les infos d'une activité en donnant son id en paramètre
+
+export async function oneIDActivites(id) {
+    const Onerecordsactivites = await pb.collection('Activites').getOne(id);
+    return Onerecordsactivites;
+}
+
+
+//une fonction qui retourne les infos d'un acteur / réalisateur en donnant son id en paramètre
+
+export async function oneIDInvites(id) {
+    const OnerecordsInvites = await pb.collection('Invites').getOne(id);
+    return OnerecordsInvites;
+}
+
+//Une fonction qui retourne toutes les activités d’un animateur donné par son id
+
+export async function AllActivitesByid(id) {
+    const Recordsactivites = await pb.collection('Activites').getFullList({ filter: `Invites.id= '${id}'`, expand: 'Invites', });
+
+
+    return Recordsactivites;
+}
+
+//Une fonction qui retourne toutes les activités d’un animateur donné par son nom
+
+export async function AllActivitesByinvitesName(Nom) {
+    const RecordsactivitesinvitesName = await pb.collection('Activites').getFullList({ filter: `Invites.Nom= '${Nom}'`, expand: 'Invites', });
+
+
+    return RecordsactivitesinvitesName;
+}
+
+//Une fonction qui permet d’ajouter ou modifier les informations d’un film, activité ou invité
+
+//film
+
+export async function addNewFilms(Newfilm) {
+    await pb.collection('Films').create(Newfilm);
+}
+
+
+export async function updateFilmsbyId(id, data) {
+    await pb.collection('Films').update(id, data);
+}
+
+
+export async function DeleteFilmbyId(id) {
+    await pb.collection('Films').delete(id);
+}
+
+
+//activites
+
+export async function addNewActivites(Newactivites) {
+    await pb.collection('Activites').create(Newactivites);
+}
+
+
+export async function updateActivitesbyId(id, data1) {
+    await pb.collection('Activites').update(id, data1);
+}
+
+export async function DeleteActivitebyId(id) {
+    await pb.collection('Activites').delete(id);
+}
+
+//invites
+
+export async function addNewInvites(Newinvites) {
+    await pb.collection('Invites').create(Newinvites);
+}
+
+
+export async function updateInvitesbyId(id, data2) {
+    await pb.collection('Invites').update(id, data2);
+}
+
+export async function DeleteInvitebyId(id) {
+    await pb.collection('Invites').delete(id);
+}
+
+
+
+
+
+
+
+
+
+export async function newUser(newuser) {
+    await pb.collection("users").create(newuser);
+}
+
+
--- backend/test-back.js
+++ backend/test-back.js
@@ -1,20 +1,213 @@
-import {  Allevents, sortedfilms } from './backend.mjs'
-
-
-
+import {  Allevents, sortedfilms, AllActivitessorted, AllInvitessorted, oneIDFilm, oneIDActivites, oneIDInvites, AllActivitesByid, AllActivitesByinvitesName} from './backend.mjs'
+import {addNewFilms,updateFilmsbyId, addNewActivites, updateActivitesbyId, addNewInvites, updateInvitesbyId, DeleteFilmbyId,DeleteActivitebyId, DeleteInvitebyId} from './backend.mjs'
+import {newUser, superUserauth} from './backend.mjs'
+/*
 try {
     const records = await Allevents() ;
     console.table(records) ;
     } catch (e) {
     console.error(e) ;
     }
+*/
+
 
     // une fonction qui retourne la liste de tous les films triés par date de projection
 
-    try {
+
+
+   /* try {
         const records = await sortedfilms();
         console.table((records));
     } catch (e) {
         console.error(e);
     }
+    */
+
+
+
+
+
+    // une fonction qui retourne la liste des activités (Ateliers, expositions, animations…) triés par date de projection    
+
+
+  /*  try {
+        const records = await AllActivitessorted();
+        console.table((records));
+    } catch (e) {
+        console.error(e);
+    }
+
+
+
+    */
+//une fonction qui retourne la liste de tous les acteurs / réalisateurs participant au festival triés par ordre alphabétique
+/*
+
+
+try {
+    const records = await AllInvitessorted();
+    console.table((records));
+} catch (e) {
+    console.error(e);
+}
+*/
+
+
+
+//une fonction qui retourne les infos d'un film en donnant son id en paramètre
+/*
+
+
+try {
+    const Onerecords = await oneIDFilm('y9lkmxpija5m6w6');
+    console.table((Onerecords));
+} catch (e) {
+    console.error(e);
+}
+*/
+
+
+
+
+//une fonction qui retourne les infos d'une activité en donnant son id en paramètre
+/*
+
+
+
+try {
+    const Onerecordsactivites = await oneIDActivites('xywx836247t352w');
+    console.table((Onerecordsactivites));
+} catch (e) {
+    console.error(e);
+}
+*/
+
+
+
+//une fonction qui retourne les infos d'un acteur / réalisateur en donnant son id en paramètre
+
+
+/*
+try {
+    const OnerecordsInvites = await oneIDInvites('axkqs94x871it97');
+    console.table((OnerecordsInvites));
+} catch (e) {
+    console.error(e);
+}
+*/
+
+
+
+
+//Une fonction qui retourne toutes les activités d’un animateur donné par son id
+/*
+
+
+
+try {
+    const Recordsactivites = await AllActivitesByid('ok2k280xnnwr641');
+    console.table((Recordsactivites));
+} catch (e) {
+    console.error(e);
+}
+*/
+
+
+
+
+//Une fonction qui retourne toutes les activités d’un animateur donné par son nom
+
+/*
+
+
+
+try {
+    const RecordsactivitesinvitesName = await AllActivitesByinvitesName("Torun");
+    console.table(RecordsactivitesinvitesName);
+} catch (e) {
+    console.error(e);
     
+}*/
+
+
+
+
+
+//Une fonction qui permet d’ajouter ou modifier les informations d’un film, activité ou invité
+
+//films
+
+const Newfilm = {
+    "Titre": "test",
+    "Genre": "horreur",
+    "Description": "test",
+  
+};
+//await addNewFilms(Newfilm);
+
+
+
+const data = {
+    "Titre": "Le chat Potté 2 ",
+    "Genre": "Animation" ,
+};
+//await updateFilmsbyId('vlhj227r9lup79u',data);
+
+//await DeleteFilmbyId ('vlhj227r9lup79u');
+
+
+
+
+
+
+// Activites 
+
+const Newactivites = {
+    "Description": "Atelier de peinture",
+    "Type": "Atelier",
+   
+};
+//await addNewActivites(Newactivites);
+
+
+const data1 = {
+    "Description": "Atelier de peinture pour les enfants",
+    
+};
+//await updateActivitesbyId('obft5av60evc1l5',data1);
+
+ //await DeleteActivitebyId('obft5av60evc1l5');
+
+
+
+
+
+// Invites
+
+const Newinvites = {
+    "Nom": "aguilar",
+    "Prenom": "Julien",
+    "Email": "julien@juju.fr",
+  
+};
+//await addNewInvites(Newinvites);
+
+
+const data2 = {
+
+    "Prenom": "Justine",
+    "Email": "justine@juju.com",
+};
+//await updateInvitesbyId('z1p54288b8pnohn',data2);
+
+//await DeleteInvitebyId('z1p54288b8pnohn');
+
+
+
+
+
+const newuser = {
+    "email": "test@test.fr",
+    "name": "Jack",
+};
+await newUser(newuser);
